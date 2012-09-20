6eac0da10fe1afbda2cfb383bc11aeac87febbee
Matthew Estes <mestes@ea.com>
2012-09-19 23:25:25 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,8 @@ describe User do
                      password: "foobar", password_confirmation: "foobar")
   end
 
+  
+
   subject { @user }
 
   it { should respond_to(:name) }
@@ -15,8 +17,18 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) } 
+
 
   it { should be_valid }
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
   describe "when name is not present" do
     before { @user.name = " " }
